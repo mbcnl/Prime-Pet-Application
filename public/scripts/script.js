cfb83261d25eae9aa0b24a3b17b1472bcdcb8ac5
mablchnole <mablchnole@gmail.com>
2016-06-26 14:24:35 -0500

    update html and add partials

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,10 @@
-console.log('script.js success');
+// define an angular module
+var gotchaApp = angular.module('gotchaApp', []);
+
+gotchaApp.controller('addController', function($scope){
+
+});
+
+gotchaApp.controller('showController', function($scope){
+  
+});
